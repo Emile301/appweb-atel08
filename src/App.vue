8ad9d213468f62a6125c72b2d8fd989d03c8d751
Emile301 <2351795@csfoy.ca>
2025-02-20 13:16:13 -0500

    first commir

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Timeline from './components/Timeline.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+</script>
+
+<template>
+  <Timeline />
+</template>
+
+<style scoped> 
+</style>
